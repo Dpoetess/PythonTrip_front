3259a932ee78a7efdf2009a7c2dffc7bb2373c4c
Erieltxu <ld.aldecoa@gmail.com>
2024-08-26 19:28:17 +0200

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import UseApi from './useApi'
-
+// App.jsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import UseApi from './useApi';
+import Navbar from './components/nav/Nav'; // Asegúrate de que la ruta sea correcta
 
 function App() {
-    return (
-      <div>
-        <h1>Connect Front to Back</h1>
-        <UseApi />
-      </div>
-  )
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<UseApi />} />
+        {/* Agrega otras rutas aquí */}
+        <Route path="/login" element={<div>Login Page</div>} />
+        <Route path="/signup" element={<div>Sign Up Page</div>} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
