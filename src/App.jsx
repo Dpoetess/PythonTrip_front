f65c585956639b54e55baf2800fc7b208549986e
CIBA <sanchezmariabelen01@gmail.com>
2024-08-27 15:46:38 +0200

    add Destination Selection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import UseApi from './useApi'
+import React from 'react';
+import UseApi from './useApi';
+import Footer from './components/Footer/Footer';
+import Dropdown from "./components/dropdown/Dropdown";
+
 
 
 
 function App() {
-    return (
-      <div>
-        <h1>Connect Front to Back</h1>
-        <UseApi />
-        
-      </div>
+
+  return (
+    <div>
+      <h1>Connect Front to Back</h1>
+      <UseApi />
+      <Dropdown />
+      <Footer />
+    </div>
   )
 }
 
